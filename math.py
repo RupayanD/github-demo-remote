8752bfca7d5dd81ea53b49d5a438c3206ed45f9e
RupayanD <93157690+RupayanD@users.noreply.github.com>
2022-03-04 13:55:24 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+#add implementation
+def add(x,y):
+  return x + y
+#add subract
+def subtract(x,y):
+ return x-y #on master
+ #add multiplication
+def multiply(x,y):
+ return x * y #on bug456
+ #Divide implementation
+def divide(x,y):
+ if(y!=0)
+ return x/y #bug789
+def square:
+pass
