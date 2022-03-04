8fb9c73848c35429fd885c641453834f10471884
Rup <rupayandeb1999@gmail.com>
2022-03-04 16:10:49 +0530

    Added new

--- math.py
+++ math.py
@@ -1,13 +1,13 @@
-#add implementation
+#add implementation new
 def add(x,y):
   return x + y
-#add subract
+#add subract new
 def subtract(x,y):
  return x-y #on master
- #add multiplication
+ #add multiplication new
 def multiply(x,y):
  return x * y #on bug456
- #Divide implementation
+ #Divide implementation new
 def divide(x,y):
  if(y!=0)
  return x/y #bug789
